b438c1f20f2afcbd88c6420edecf533b801f728c
GuJiaHeng <709259059@qq.com>
2019-08-26 17:39:11 +0800

    第二次测试Url

--- src/TestGit2.java
+++ src/TestGit2.java
@@ -2,5 +2,6 @@ public class TestGit2 {
     public static void main(String[] args) {
         System.out.println("测试Git2");
         System.out.println("测试Git3");
+        System.out.println("测试Git4");
     }
 }
