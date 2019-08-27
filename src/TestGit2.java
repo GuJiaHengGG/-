b4fd6319054de00a2dd89483f470de6790c27eeb
GuJiaHeng <709259059@qq.com>
2019-08-27 09:47:30 +0800

    删除

--- src/TestGit2.java
+++ src/TestGit2.java
@@ -1,8 +1,5 @@
 public class TestGit2 {
     public static void main(String[] args) {
-        System.out.println("测试Git2");
-        System.out.println("测试Git3");
-        System.out.println("测试Git4");
         System.out.println("测试Git5");
     }
 }
